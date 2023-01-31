e7d241661994a1f897b945278c29318d15679c87
Bekmyrza <msn.10110922@gmail.com>
2023-01-31 15:38:16 +0600

    Add the basic structure of the application

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
+import Navigation from "./components/Navigation"
+import FavoritesPage from "./pages/FavoritesPage"
+import HomePage from "./pages/HomePage"
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/FavoritesPage.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const FavoritesPage = () => {
+  return <div>fav</div>
+}
+
+export default FavoritesPage
